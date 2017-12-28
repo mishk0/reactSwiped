44d094c7eedcfd983abe92debaab48cb3b7af58e
mishk0 <mokrushin.ms@yandex.ru>
2017-12-28 19:42:06 +0300

    add react

--- example/example.js
+++ example/example.js
@@ -1,4 +1,6 @@
 import Swiped from '../src';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 Swiped.init({
     elem: '.list1',
@@ -32,3 +34,11 @@ Swiped.init({
         console.log('move');
     }
 });
+
+const App = () => (
+    <div className="App">
+        <span>React Swipe</span>
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
